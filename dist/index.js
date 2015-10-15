44f14207df6158b3504751eac75702dc342453c6
meng <pingf0@gmail.com>
2015-10-15 21:47:40 +0800

    remove dist folder

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-webpackJsonp([2],[
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
-
-	eval("'use strict';\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }\n\nvar _react = __webpack_require__(2);\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _componentsButton = __webpack_require__(1);\n\nvar _componentsButton2 = _interopRequireDefault(_componentsButton);\n\nvar _componentsButton0 = __webpack_require__(158);\n\nvar _componentsButton02 = _interopRequireDefault(_componentsButton0);\n\n_react2['default'].render(_react2['default'].createElement(\n    'div',\n    null,\n    _react2['default'].createElement(\n        _componentsButton2['default'],\n        { onclick: clickHandler },\n        'click me123'\n    ),\n    _react2['default'].createElement(\n        _componentsButton02['default'],\n        { onclick: clickHandler },\n        'click me again'\n    )\n), document.getElementById('app'));\n\nfunction clickHandler() {\n    console.log('haha123');\n}\n\n/*****************\n ** WEBPACK FOOTER\n ** ./src/index.js\n ** module id = 0\n ** module chunks = 2\n **/\n//# sourceURL=webpack:///./src/index.js?");
-
-/***/ }
-]);
