c29910537a5f318ddf27b8d89ffe033441756f87
pingf <pingf0@gmail.com>
2015-10-20 17:40:07 +0800

    add custom style

--- src/components/button.js
+++ src/components/button.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 class Button extends React.Component {
+	constructor(){
+		super();
+		this.style = {
+		  color: 'green',
+		};
+	}
 	render() {
 		return (
-			<button onClick={this.props.onClick}>{this.props.children}</button>
+			<button onClick={this.props.onClick} style={this.style}>{this.props.children}</button>
 		);
 	}
 }
 
 export default Button;
+
+
