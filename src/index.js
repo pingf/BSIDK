f7d84e9d57ccc7fe052af14324dce02a92c46476
pingf <pingf0@gmail.com>
2015-11-02 16:57:58 +0800

    add material-ui

--- src/index.js
+++ src/index.js
@@ -6,18 +6,26 @@
 // 	</div>
 // ) , document.getElementById('app'));
 
+import mui from 'material-ui';
+// let MenuItem = mui.MenuItem;
+// let LeftNav = mui.LeftNav;
+let RaisedButton = mui.RaisedButton;
 
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 
-class Hello extends React.Component {
+class Hello extends React.Component{ 
   render() {
     return <h1>Hello world</h1>;
   }
 }
 
 ReactDOM.render(
-  <Hello />,
+  	<div>
+	  <Hello />
+	  <RaisedButton label="hello" primary={true}/>
+  	</div>
+  ,
   document.getElementById('app')
 );
